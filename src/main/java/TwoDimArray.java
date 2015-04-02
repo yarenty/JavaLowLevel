4a2fea72291f5ae958cd7808ed832362fdf5db76
yarenty <yarenty@gmail.com>
2015-04-02 16:53:22 +0100

    Two Dimensional array

---
+++ src/main/java/TwoDimArray.java
@@ -0,0 +1,27 @@
+
+public class TwoDimArray {
+
+	public static void main(String[] args) {
+		
+		    int column;
+		    int row;
+
+		    String[][] newArray = {{"Poland","Red"},{"Ireland","Green"}};
+
+		    for (String line[]:newArray){
+		    	for(String element:line){
+		    		System.out.print(element+"\t");
+		    	}
+		    	System.out.println();
+		    }
+		    
+		    
+		    for(row = 0; row < newArray.length; row++){
+		        for(column = 0; column < newArray.length;column++){
+		            System.out.print(newArray[row][column]+"\t");
+		        }
+		        System.out.println();
+		    }
+		           
+	}
+}
