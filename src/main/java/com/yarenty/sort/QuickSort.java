30e2f3a18cca3e364d053599756d68578911d6b1
yarenty <yarenty@gmail.com>
2015-04-07 15:22:37 +0100

    sort...

--- src/main/java/com/yarenty/sort/QuickSort.java
+++ src/main/java/com/yarenty/sort/QuickSort.java
@@ -1,70 +1,69 @@
 package com.yarenty.sort;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
-
 
 /**
+ * INPUT
+ <pre>
+ 23, 44, 1, 2009, 2, 88, 123, 7, 999, 1040, 88
+ </pre>
  * OUTPUT:
-<pre>
-
-</pre>
+ <pre>
+ 1 2 7 23 44 88 88 123 999 1040 2009
+ </pre>
  * @author yarenty
  *
  */
 public class QuickSort {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-		// int[] a = { 1, 23, 45, 2, 8, 134, 9, 4, 2000 };
-		int a[] = { 23, 44, 1, 2009, 2, 88, 123, 7, 999, 1040, 88 };
-		quickSort(a, 0, a.length - 1);
-		for (int i : a) {
-			System.out.print(i+" ");
-		}
-		Deque<Integer> stack = new ArrayDeque<Integer>();
-		Queue<Integer> q = new ArrayBlockingQueue<Integer>(0);
-		
-	}
+        // int[] a = { 1, 23, 45, 2, 8, 134, 9, 4, 2000 };
+        int a[] = { 23, 44, 1, 2009, 2, 88, 123, 7, 999, 1040, 88 };
+        quickSort(a, 0, a.length - 1);
+        for (int i : a) {
+            System.out.print(i+" ");
+        }
+//        Deque<Integer> stack = new ArrayDeque<Integer>();
+//        Queue<Integer> q = new ArrayBlockingQueue<Integer>(0);
 
-	public static void quickSort(int[] a, int p, int r) {
-		if (p < r) {
-			int q = partition(a, p, r);
-			quickSort(a, p, q);
-			quickSort(a, q + 1, r);
-		}
-	}
+    }
 
-	private static int partition(int[] a, int p, int r) {
+    public static void quickSort(int[] a, int p, int r) {
+        if (p < r) {
+            int q = partition(a, p, r);
+            quickSort(a, p, q);
+            quickSort(a, q + 1, r);
+        }
+    }
 
-		int x = a[p];
-		int i = p - 1;
-		int j = r + 1;
+    private static int partition(int[] a, int p, int r) {
 
-		while (true) {
-			i++;
-			while (i < r && a[i] < x)
-				i++;
-			j--;
-			while (j > p && a[j] > x)
-				j--;
+        int x = a[p];
+        int i = p - 1;
+        int j = r + 1;
 
-			if (i < j)
-				swap(a, i, j);
-			else
-				return j;
-		}
-	}
+        while (true) {
+            i++;
+            while (i < r && a[i] < x)
+                i++;
+            j--;
+            while (j > p && a[j] > x)
+                j--;
 
-	private static void swap(int[] a, int i, int j) {
-		int temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
+            if (i < j)
+                swap(a, i, j);
+            else
+                return j;
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
 }
