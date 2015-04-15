16f8ae75a80bfc0d7a5d2fb78e56f22d605f21ea
yarenty <yarenty@gmail.com>
2015-04-15 11:34:50 +0100

    recursive permutations

---
+++ src/main/java/com/yarenty/strings/RecursivePermutations.java
@@ -0,0 +1,18 @@
+package com.yarenty.strings;
+
+/**
+ * Write a recursive function for generating all permutations of an input string. Return them as an array.
+ * Don't worry about duplicates—assume every character is unique.
+ * <p/>
+ * Don't worry about time or space complexity—if we wanted efficiency we'd write an iterative version.
+ * <p/>
+ * Your function can have loops—it just needs to also be recursive.
+ * <p/>
+ * Created by yarenty on 15/04/15.
+ */
+public class RecursivePermutations {
+
+
+
+
+}
