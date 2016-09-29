634db49b3a4e92a6d7c654c856778df41eebbd80
yarenty <yarenty@gmail.com>
2016-09-29 14:51:33 +0100

    class wrappers

---
+++ src/main/java/com/yarenty/quiz/WraperTest.java
@@ -0,0 +1,18 @@
+package com.yarenty.quiz;
+
+/**
+ * Created by yarenty on 29/09/2016.
+ * (C)2015 SkyCorp Ltd.
+ */
+public class WraperTest {
+
+    public static void main(String[] args) {
+
+//        Wrapper.Wrapped ww1 = new Wrapper.Wrapped();
+//        Wrapper.Wrapped ww2 = new Wrapper().Wrapped();
+        Wrapper.Wrapped ww3 = new Wrapper().new Wrapped();
+//        Wrapper.Wrapped ww4 = new Wrapper::Wrapped();
+
+    }
+
+}
---
+++ src/main/java/com/yarenty/quiz/Wrapper.java
@@ -0,0 +1,11 @@
+package com.yarenty.quiz;
+
+/**
+ * Created by yarenty on 29/09/2016.
+ * (C)2015 SkyCorp Ltd.
+ */
+public class Wrapper {
+
+    public class Wrapped {}
+
+}
